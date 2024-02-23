b89ac329757709cbd9bb91f5f9451868be16b8bd
Borja Martí <borjamarti@outlook.com>
2024-02-23 23:58:40 +0100

    initial tanstack router/query setup, pokeball favicon

---
+++ src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import {
+  Link,
+  Outlet,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  );
+}
