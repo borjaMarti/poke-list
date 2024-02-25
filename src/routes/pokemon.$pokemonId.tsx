736668122d82edd39f8ac45e3d39a14bd3435f66
Borja Martí <borjamarti@outlook.com>
2024-02-25 22:18:49 +0100

    refactored pokemon id page

--- src/routes/pokemon.$pokemonId.tsx
+++ src/routes/pokemon.$pokemonId.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, notFound } from "@tanstack/react-router";
-import useCapturedStore from "src/stores/use-captured-store";
-import usePokemonQuery from "src/hooks/use-pokemon-query";
 import { Typography } from "@mui/material";
+import PokemonDetails from "src/components/pokemon-details";
 
 export const Route = createFileRoute("/pokemon/$pokemonId")({
   component: Pokemon,
@@ -21,36 +20,6 @@ export const Route = createFileRoute("/pokemon/$pokemonId")({
 
 function Pokemon() {
   const { pokemonId } = Route.useParams();
-  const { data: pokemon, isSuccess } = usePokemonQuery(+pokemonId);
-  const { capturedPokemon, capturePokemon, releasePokemon } =
-    useCapturedStore();
 
-  return (
-    <div className="p-2">
-      {isSuccess && (
-        <>
-          <img
-            src={
-              pokemon.pokemon_v2_pokemon[0].pokemon_v2_pokemonsprites[0].sprites
-            }
-          />
-          <h3>
-            {
-              pokemon.pokemon_v2_pokemon[0].pokemon_v2_pokemonspecy
-                ?.pokemon_v2_pokemonspeciesnames[0].name
-            }
-          </h3>
-          <button
-            onClick={() => {
-              if (capturedPokemon.has(+pokemonId)) releasePokemon(+pokemonId);
-              else capturePokemon(+pokemonId);
-            }}
-          >
-            CAPTURAR
-          </button>
-          {capturedPokemon.has(+pokemonId) ? "Capturado" : "Libre"}
-        </>
-      )}
-    </div>
-  );
+  return <PokemonDetails pokemonId={+pokemonId} />;
 }
