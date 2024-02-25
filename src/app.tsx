9d69860cf2365f3589676129be4470ada57611a0
Borja Martí <borjamarti@outlook.com>
2024-02-25 11:28:35 +0100

    added tanstack devtools

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // TanStack Router: Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -42,6 +43,7 @@ if (!rootElement.innerHTML) {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </StrictMode>,
   );
