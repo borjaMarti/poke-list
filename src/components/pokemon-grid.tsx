fe284060d126c34c405304a3c3f8667ac231d0ba
Borja Martí <borjamarti@outlook.com>
2024-02-25 21:23:17 +0100

    refactored pokemon grid component

--- src/components/pokemon-grid.tsx
+++ src/components/pokemon-grid.tsx
@@ -3,33 +3,45 @@ import usePokemonListQuery from "src/hooks/use-pokemon-list-query";
 import useCapturedStore from "src/stores/use-captured-store";
 import PokemonCard from "./pokemon-card";
 import { CircularProgress } from "@mui/material";
+import { Typography } from "@mui/material";
 
 interface PokemonGridProps {
   filtered: boolean;
 }
 
 const PokemonGrid = ({ filtered }: PokemonGridProps) => {
-  const { data: pokemonList, isSuccess } = usePokemonListQuery();
+  const {
+    data: pokemonList,
+    isSuccess,
+    isError,
+    isLoading,
+  } = usePokemonListQuery();
   const { capturedPokemon } = useCapturedStore();
 
-  return isSuccess ? (
-    <Grid container spacing={3} justifyContent="center">
-      {pokemonList.pokemon_v2_pokemon.map((pokemon) => {
-        // Borja: If we are in the /captured page, only show pokemons that have been captured.
-        if (filtered && !capturedPokemon.has(pokemon.id)) {
-          return;
-        } else {
-          return (
-            <Grid key={pokemon.id}>
-              <PokemonCard pokemon={pokemon} filtered={filtered} />
-            </Grid>
-          );
-        }
-      })}
-    </Grid>
-  ) : (
-    <CircularProgress color="secondary" />
-  );
+  if (isLoading) return <CircularProgress color="secondary" />;
+  if (isSuccess)
+    return (
+      <Grid container spacing={3} justifyContent="center">
+        {pokemonList.pokemon_v2_pokemon.map((pokemon) => {
+          // Borja: If we are in the /captured page, only show pokemons that have been captured.
+          if (filtered && !capturedPokemon.has(pokemon.id)) {
+            return;
+          } else {
+            return (
+              <Grid key={pokemon.id}>
+                <PokemonCard pokemon={pokemon} filtered={filtered} />
+              </Grid>
+            );
+          }
+        })}
+      </Grid>
+    );
+  if (isError)
+    return (
+      <Typography variant="h4" component="p">
+        Parece que la Pokédex está saturada...
+      </Typography>
+    );
 };
 
 export default PokemonGrid;
